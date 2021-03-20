3d61b9144e6644e1136d0e1470dd00bd0a39b67a
CharlesLee01 <77286511+CharlesLee01@users.noreply.github.com>
2021-03-20 11:37:38 +0800

    Revert "Documentation update"
    This reverts commit f73f9eff95f0036ad8c3447fca0d17f0d2f98dfe.

--- src/main/java/dog/pawbook/logic/commands/DeleteCommand.java
+++ src/main/java/dog/pawbook/logic/commands/DeleteCommand.java
@@ -6,10 +6,9 @@ public abstract class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the owner/dog/program identified by the index number used in the displayed entity list.\n"
+            + ": Deletes the owner/dog/program identified by the index number used in the displayed owner list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " owner 1\n"
-            + "Example: " + COMMAND_WORD + " dog 1";
+            + "Example: " + COMMAND_WORD + " owner 1";
 
     public static final String MESSAGE_DELETE_SUCCESS_FORMAT = "Deleted %s: ";
 
